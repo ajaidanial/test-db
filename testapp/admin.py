d699a9b91f90e287f4d0e40aad0b6b2455fe685a
ajaidanial <ajaidanial@gmail.com>
2019-05-25 00:01:12 +0530

    test 3

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from testapp.models import User, TaskList, Task
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(TaskList)
+admin.site.register(Task)
--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from typing import List
 
-# Create your views here.
+from testapp.models import *
+
+
+def clear_all_tasks_for_user(name: str) -> None:
+    user = User.objects.get(name=name)
+    for task in user.assigned_tasks.all():
+        task.is_open = False
+        task.save()
+
+
+def has_user_open_tasks(name: str) -> List[str]:
+    user = User.objects.get(name=name)
+    open_tasks = []
+    for task in user.assigned_tasks.all():
+        if task.is_open:
+            open_tasks.append(task.name)
+    return open_tasks
+
+
+def tasks_created_by_user(name: str) -> List[str]:
+    user = User.objects.get(name=name)
+    created_tasks = []
+    for task in user.assigned_tasks.all():
+        if task.creator.name == user.name:
+            created_tasks.append(user.name)
+    return created_tasks
